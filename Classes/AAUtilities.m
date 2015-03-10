a1cbb8cef462457200658a3826b64d9f46886c57
Akil Ash <akil@ashinteractive.com>
2015-03-10 15:58:55 -0400

    Adding system version macros

---
+++ Classes/AAUtilities.m
@@ -0,0 +1,13 @@
+//
+//  AAUtilities.m
+//  AshUtilities
+//
+//  Created by Akil Ash on 3/10/15.
+//  Copyright (c) 2015 Akil Ash. All rights reserved.
+//
+
+#import "AAUtilities.h"
+
+@implementation AAUtilities
+
+@end
