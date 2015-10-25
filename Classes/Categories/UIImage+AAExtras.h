3452f157196949a4c9a82200d71b0a287e5d4a8c
Akil Ash <akil.ash@huffingtonpost.com>
2015-10-25 01:54:43 -0400

    Creating a UIImage categorical extension

---
+++ Classes/Categories/UIImage+AAExtras.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+AAExtras.h
+//  Reddera
+//
+//  Created by Ash, Akil on 10/25/15.
+//  Copyright © 2015 Reddera Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (AAExtras)
+
+@end
---
+++ Classes/Categories/UIImage+AAExtras.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+AAExtras.m
+//  Reddera
+//
+//  Created by Ash, Akil on 10/25/15.
+//  Copyright © 2015 Reddera Inc. All rights reserved.
+//
+
+#import "UIImage+AAExtras.h"
+
+@implementation UIImage (AAExtras)
+
+@end
