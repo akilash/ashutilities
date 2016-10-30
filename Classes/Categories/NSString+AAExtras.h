fe70b60c955637b229412d43f39ebcdf2436f6b5
Akil Ash <akil.ash@huffingtonpost.com>
2016-10-29 20:58:33 -0400

    Introducing a categorical extension to NSString

---
+++ Classes/Categories/NSString+AAExtras.h
@@ -0,0 +1,13 @@
+//
+//  NSString+AAExtras.h
+//  Reddera
+//
+//  Created by Ash, Akil on 10/20/16.
+//  Copyright © 2016 Reddera Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (AAExtras)
+
+@end
---
+++ Classes/Categories/NSString+AAExtras.m
@@ -0,0 +1,13 @@
+//
+//  NSString+AAExtras.m
+//  Reddera
+//
+//  Created by Ash, Akil on 10/20/16.
+//  Copyright © 2016 Reddera Inc. All rights reserved.
+//
+
+#import "NSString+AAExtras.h"
+
+@implementation NSString (AAExtras)
+
+@end
